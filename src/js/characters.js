4b6764e678276fd2fc9f5be58684c2abc7a3bddc
Polina Gorelik <Polina23707@gmail.com>
2023-12-12 14:21:43 +0200

    4th commit

--- src/js/characters.js
+++ src/js/characters.js
@@ -1,21 +1,6 @@
 export class Characters {
   constructor(name) {
     this.name = name;
-    // this.demage= 100;
-    this.health = 100;
-  }
-  
-  get stoned() {
-    return this._stoned;
-  }
-
-  set stoned(location) {
-    let stonedLevel = new Map([[1, 1] , [2, 0.9] ,[3, 0.8] , [4, 0.7] , [5, 0.6]]);
-    console.log(stonedLevel.get(location));
-    this._stoned = this.health * stonedLevel.get(location);
-    console.log(this._stoned);
-    this.health = this.health - this._stoned;
-    console.log(this.health);
   }
 
   get attack() {
@@ -23,7 +8,8 @@ export class Characters {
   }
 
   set attack(location) {
-    let dopeLevel = Math.log2(location) * 5;
-    this._attack = dopeLevel ;
+    let attackLevel = new Map([[1, 1] , [2, 0.9] ,[3, 0.8] , [4, 0.7] , [5, 0.6]]);
+    // console.log(attackLevel.get(location));
+    this._attack = attackLevel.get(location);
   }
 }
--- src/js/magician.js
+++ src/js/magician.js
@@ -4,4 +4,14 @@ export class Magician extends Characters {
   constructor(name) {
     super(name);
   }
+
+  get stoned() {
+    return this._stoned;
+  }
+
+  set stoned(location) {
+    let attackLevel = new Map([[1, 1] , [2, 0.9] ,[3, 0.8] , [4, 0.7] , [5, 0.6]]);
+    this._stoned = attackLevel.get(location) - Math.log2(location) * 5;
+    console.log(this._stoned);
+  }  
 }
